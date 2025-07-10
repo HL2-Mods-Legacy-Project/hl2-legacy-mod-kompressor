535c9f3ebb485a0f7734425c3c01966695350ed5
Marc-Antoine Lortie <malortie@users.noreply.github.com>
2025-07-09 20:02:12 -0400

    Update build_all.ps1

--- scripts/build_all.ps1
+++ scripts/build_all.ps1
@@ -1,12 +1,10 @@
 & "$PSScriptRoot\..\gitmodules\mod-installer\build_all.ps1" `
-  -Version "<version>" `
+  -Version "2.0.0" `
   -PreReleaseVersion "beta.1" `
-  -OutputFilePrefix "<output file prefix>" `
-  -ModName "<mod name>" `
-  -ModFolder "<mod folder>" `
-  -AdditionalModFolders @() `
+  -OutputFilePrefix "Kompressor" `
+  -ModName "Kompressor" `
+  -ModFolder "Kompressor" `
   -ModFilesPath "$PSScriptRoot\..\game" `
-  -ModBaseFilesUrl "<url to mod base files zip>" `
-  -ModBaseFilesUrlHash "<mod base files zip sha256>" `
-  -ModReadmePath "<mod readme path>" `
+  -ModBaseFilesUrl "https://github.com/HL2-Mods-Legacy-Project/hl2-legacy-registry/releases/download/kompressor/Kompressor-Original.zip" `
+  -ModBaseFilesUrlHash "d542b0e162227c28f98a47b1a16f949cdf8d9cc5e7df7f01ab0650a6c9cc98d3" `
   -PublishPath "$PSScriptRoot\..\artifacts\publish"
